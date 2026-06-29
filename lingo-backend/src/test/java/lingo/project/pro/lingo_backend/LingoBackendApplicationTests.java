a81d298e47e8b43cd560b347c21c853b217a4ec3
Maciej Matusz <86675591+maciejmatusz@users.noreply.github.com>
2026-06-29 18:38:57 +0200

    #3 set up mono repo (#5)

---
+++ lingo-backend/src/test/java/lingo/project/pro/lingo_backend/LingoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lingo.project.pro.lingo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
