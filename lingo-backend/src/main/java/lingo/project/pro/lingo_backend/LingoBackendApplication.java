a527a1a987f255eb2e0dfe7b45b8792d90321310
Maciej Matusz <maciej.matusz.szczecin@gmail.com>
2026-06-29 18:37:45 +0200

    #3 set up mono repo

---
+++ lingo-backend/src/main/java/lingo/project/pro/lingo_backend/LingoBackendApplication.java
@@ -0,0 +1,13 @@
+package lingo.project.pro.lingo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingoBackendApplication.class, args);
+	}
+
+}
